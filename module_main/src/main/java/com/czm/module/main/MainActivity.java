563f2a86f5c7ef61ee3f650a9ef6cbbb552552dc
czm <1010446974@qq.com>
2018-04-08 11:17:32 +0800

    module_grendao initial commit

--- module_main/src/main/java/com/czm/module/main/MainActivity.java
+++ module_main/src/main/java/com/czm/module/main/MainActivity.java
@@ -6,7 +6,7 @@ import android.widget.Button;
 
 import com.alibaba.android.arouter.launcher.ARouter;
 
-public class MainActivity extends AppCompatActivity {
+public class  MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
